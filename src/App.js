a90da6df4a50c6b95d04f6611a6bcff42f3ed984
JAlan <GeeWhyAlan@outlook.com>
2024-07-25 01:28:01 -0500

    add navbar

--- src/App.js
+++ src/App.js
@@ -3,17 +3,18 @@ import Demo from "./pages/demo/demo"
 import Home from "./pages/home/home"
 import Navbar from "./components/public/navbar/navbar"
 
+import './App.css'
 
 function App() {
 
   return (
-          <>
+          <div className="App">
           <Navbar/>
           <Routes>
             <Route exact path="/" element={<Home/>}/>
             <Route path="/demo" element={<Demo/>}/>
           </Routes>
-          </>
+          </div>
 
   );
 }
--- src/components/public/navbar/navbar.jsx
+++ src/components/public/navbar/navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react"
+import React, { useEffect, useState } from "react"
 
 import "./style.css"
 import { Link } from "react-router-dom"
@@ -6,9 +6,25 @@ import { Link } from "react-router-dom"
 
 
 export default function Navbar() {
+
+  useEffect(() => {
+    var prevScroll = 0;
+    var currentScroll = 0;
+    window.addEventListener('scroll', () => {
+      currentScroll = window.scrollY;
+      if (prevScroll > currentScroll) {
+        document.getElementById("navbar").style.top = "0";
+      } else {
+        document.getElementById("navbar").style.top = "-50px";
+      }
+      prevScroll = currentScroll
+    })
+  }, [])
+    
+    
     
     return (
-        <div className={"navbar"} data-scroll-section>
+        <div id={"navbar"}>
             <button onClick={() => {}} id="link">About</button>
             <button onClick={() => {}} id="link">Credit</button>
             <Link to="/demo" id="link">Try It!</Link>
