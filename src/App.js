4048972fdbfeecb2a0dc74c3c2a6f456b2ede1be
Pronkle <allenleiguo@gmail.com>
2024-07-24 22:27:45 -0500

    cleaned

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>HELLO</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
