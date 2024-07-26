ee0ef6bea69cdc231c566e04215027af97b8a40a
JAlan <GeeWhyAlan@outlook.com>
2024-07-26 11:52:18 -0500

    fix openai

--- src/pages/lesson/lesson.jsx
+++ src/pages/lesson/lesson.jsx
@@ -1,35 +1,27 @@
 import './lesson.css';
 import { useEffect } from 'react';
 import Menu from '../../components/demo/menu/menu';
-import { Configuration, OpenAIApi } from "openai"
+import OpenAI from "openai";
+
 
 const API_KEY = process.env.REACT_APP_API_KEY
 
+
 export default function Lesson() {
 
-    let configuration = new Configuration({ apiKey: API_KEY,});
+    const openai = new OpenAI({apiKey: API_KEY});
 
-    // Delete it
-    delete configuration.baseOptions.headers['User-Agent'];
-
-    const openai = new OpenAIApi(configuration);
 
     const handleSubmit = async (prompt) => {
         
       try {
-        //console.log("Prompt: " + prompt)
-        const result = await openai.createChatCompletion({
-          model: "gpt-3.5-turbo",
-        //   model: "text-davinci-003",
-          messages: [{ role: "user", content: prompt }],
-        //   prompt: prompt,
-        //   temperature: 0.3,
-        //   max_tokens: 500,
-        });
-        //console.log("response", result.data.choices[0].text);
-        // setApiResponse(result.data.choices[0].text);
-        //console.log("result: "+ result.data.choices[0].message.content)
-        return result.data.choices[0].message.content  
+        const result = await openai.chat.completions.create({
+            messages: [{ role: "user", content: prompt }],
+            model: "gpt-4o-mini",
+          });
+        
+
+          return result.data.choices[0].message.content  
       } catch (e) {
         console.log(e);
         return "Something went wrong, try again..."
