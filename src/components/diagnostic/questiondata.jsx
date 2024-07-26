8fe7dcf593aed519048946e5946018838cb4c15f
Pronkle <allenleiguo@gmail.com>
2024-07-26 16:01:02 -0500

    question data

--- src/components/diagnostic/questiondata.jsx
+++ src/components/diagnostic/questiondata.jsx
@@ -1,5 +1,65 @@
 import React from 'react'
 
 const QuestionData = [
+    {
+        id: 0, 
+        title: "Demo Question",
+        options: ["o1", "o2", "o3", "o4"],
+        ans: 1
+    },
+    {
+        id: 1, 
+        title: "Demo Question",
+        options: ["o1", "o2", "o3", "o4"],
+        ans: 1
+    },
+    {
+        id: 2, 
+        title: "Demo Question",
+        options: ["o1", "o2", "o3", "o4"],
+        ans: 1
+    },
+    {
+        id: 3, 
+        title: "Demo Question",
+        options: ["o1", "o2", "o3", "o4"],
+        ans: 1
+    },
+    {
+        id: 4, 
+        title: "Demo Question",
+        options: ["o1", "o2", "o3", "o4"],
+        ans: 1
+    },
+    {
+        id: 5, 
+        title: "Demo Question",
+        options: ["o1", "o2", "o3", "o4"],
+        ans: 1
+    },
+    {
+        id: 6, 
+        title: "Demo Question",
+        options: ["o1", "o2", "o3", "o4"],
+        ans: 1
+    },
+    {
+        id: 7, 
+        title: "Demo Question",
+        options: ["o1", "o2", "o3", "o4"],
+        ans: 1
+    },
+    {
+        id: 8, 
+        title: "Demo Question",
+        options: ["o1", "o2", "o3", "o4"],
+        ans: 1
+    },
+    {
+        id: 9, 
+        title: "Demo Question",
+        options: ["o1", "o2", "o3", "o4"],
+        ans: 1
+    },
 ]
 export default QuestionData
