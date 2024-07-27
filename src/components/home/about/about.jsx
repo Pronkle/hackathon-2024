6a42355d912943f3374c7245b99dc9d81b9ae746
JAlan <GeeWhyAlan@outlook.com>
2024-07-27 09:25:33 -0500

    add animations

--- src/components/home/about/about.jsx
+++ src/components/home/about/about.jsx
@@ -10,6 +10,28 @@ export default function About() {
     useLayoutEffect(() => {
         let ctx = gsap.context(() => {
             gsap.registerPlugin(ScrollTrigger)
+
+            gsap.from("#about-title", {
+                scrollTrigger: {
+                    trigger: '.about-section',
+                    start: "top top",
+                    end: "+=300",
+                    scrub: true,
+                },
+                opacity: 0,
+                transform: "translate(0, -200px)",
+            })
+
+            gsap.from("#about-description", {
+                scrollTrigger: {
+                    trigger: '.about-section',
+                    start: "top top",
+                    end: "+=500",
+                    scrub: true,
+                },
+                opacity: 0,
+                transform: "translate(0, 200px)",
+            })
             
             gsap.from('.about-info', {
                 scrollTrigger: {
@@ -28,8 +50,8 @@ export default function About() {
     return (
         <div className="about-section">
             <div className="about-info">
-                <h1 id="about-title">Our Promise</h1>
-                <div id="about-description">We aim to provide quality education, free for all, in order to not have another crowdstrike outage lol.</div>
+                <h1 id="about-title">Empower Minds. Education for All.</h1>
+                <div id="about-description">Flex Ed is an app with an interactive AI that is designed to be highly adaptable to a child's individual needs. The AI takes the local area or specific school that the child is being prepared for, takes the education plan from that area or school, then tutors your child in those areas. This help bridge education gaps from child to child and elevate your child to be a more active learner. The AI can take on any role, from a helper to raise your child's grades, to a tutor to teach your child subjects that they are lacking in.</div>
             </div>
         </div>
     )
