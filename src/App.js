134f7cc666552d035a85951c631a1ec9ba5c6de6
Pronkle <allenleiguo@gmail.com>
2024-07-26 23:17:26 -0500

    updated routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { Route, Routes} from "react-router-dom"
-import Demo from "./pages/demo/demo"
 import Home from "./pages/home/home"
 import Diagnostic from "./pages/diagnostic/diagnostic"
 import Schedule from "./pages/schedule/schedule"
@@ -17,7 +16,6 @@ function App() {
     <div className="App">
         <Routes>
           <Route exact path="/" element={<Home/>}/>
-          <Route path="/demo" element={<Demo/>}/>
           <Route path="/diagnostic" element={<Diagnostic/>}/>
           <Route path="/schedule" element={<Schedule/>}/>
           <Route path="/lesson" element={<Lesson/>}/>
